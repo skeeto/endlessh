715f30c3a72bbaeae60bf02c704aabea8ad11c0d
Christopher Wellons <wellons@nullprogram.com>
2019-08-06 20:31:35 -0400

    Add public domain dedication to the source header

--- endlessh.c
+++ endlessh.c
@@ -1,3 +1,7 @@
+/* Endlessh: an SSH tarpit
+ *
+ * This is free and unencumbered software released into the public domain.
+ */
 #if defined(__OpenBSD__)
 #  define _BSD_SOURCE  /* for pledge(2) and unveil(2) */
 #else
